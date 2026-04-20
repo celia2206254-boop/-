cae8d23e9c0b5acef41b315645c134f15d730c8b
celia2206254-boop <celia2206254@gmail.com>
2026-04-20 23:20:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.getElementById('root').innerHTML = '<h1>看到这行字说明部署终于成功了！</h1>';
